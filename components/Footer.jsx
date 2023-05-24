81b77587ad093ca9d4cede68f0390a7b978b4028
Faiz Alam <faizalam9883@gmail.com>
2023-05-25 02:35:22 +0530

    Improved Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,20 @@
 const Footer = () => {
   return (
-    <footer className="bg-blue-700 text-center  sticky top-[100vh] border-t-4 border-blue-900">
-      <div className="p-4 text-white flex justify-around sm:uppercase">
-        <div>Abhinay Sharma</div>
-        <div>Avneesh Srivastava</div>
-        <div>Ayush Chaturvedi</div>
-        <div>Bhuiyash Srivastava</div>
-        <div>Faiz Alam</div>
+    <footer className="bg-[#384e96] text-center  sticky top-[100vh] border-t-4 border-[#2C1E95] border-dashed">
+      <div className="p-4 text-black flex justify-around text-sm sm:uppercase lg:text-base">
+        <div className="border-r-2 border-black border-solid px-2 md:border-none">
+          Abhinay Sharma
+        </div>
+        <div className="border-r-2 border-black border-solid px-2 md:border-none">
+          Avneesh Srivastava
+        </div>
+        <div className="border-r-2 border-black border-solid px-2 md:border-none">
+          Ayush Chaturvedi
+        </div>
+        <div className="border-r-2 border-black border-solid px-2 md:border-none">
+          Bhuiyash Kumar
+        </div>
+        <div className="px-2">Faiz Alam</div>
       </div>
     </footer>
   );
